fed6dc6478d35f236cf9533c98a0053292e8f4fc
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-07-07 21:26:34 +0200

    Stylizing login card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import styles from "./App.module.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <main>
       <h1>Neumorphism Log Cards</h1>
+      <Login />
     </main>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+import styles from "./Cards.module.css";
+
+function Login() {
+  return (
+    <article className={styles.logcard}>
+      <h2 tabIndex="0">
+        <span>&#11206;</span> Login
+      </h2>
+      <form className={styles.logform}>
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" />
+
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" />
+
+        <button type="button" className={styles.logbtn}>
+          Login
+        </button>
+      </form>
+    </article>
+  );
+}
+
+export default Login;
