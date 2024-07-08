ee4d37e4d1c66dee25739dbdf6306edb0c0bc4e5
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-07-08 15:06:36 +0200

    fixing keyboard navigation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,19 +12,39 @@ function Login() {
     <article
       className={`${styles.logcard} ${showLog ? styles.showcard : styles.hiddencard}`}
     >
-      <h2 tabIndex="0" className={styles.cardtitle} onClick={toggleLog}>
-        <span>&#11206;</span> Login
+      <h2 onClick={toggleLog}>
+        <button className={styles.cardtitle}>
+          <span>&#11206;</span> Sign Up
+        </button>
       </h2>
       <form
         className={`${styles.logform} ${showLog ? styles.showform : styles.hiddenform}`}
       >
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" />
+        <label tabIndex={showLog ? "0" : "-1"} htmlFor="email">
+          Email
+        </label>
+        <input
+          tabIndex={showLog ? "0" : "-1"}
+          id="email"
+          name="email"
+          type="email"
+        />
 
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
+        <label tabIndex={showLog ? "0" : "-1"} htmlFor="password">
+          Password
+        </label>
+        <input
+          tabIndex={showLog ? "0" : "-1"}
+          id="password"
+          name="password"
+          type="password"
+        />
 
-        <button type="button" className={styles.logbtn}>
+        <button
+          tabIndex={showLog ? "0" : "-1"}
+          type="button"
+          className={styles.logbtn}
+        >
           Login
         </button>
       </form>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -12,25 +12,59 @@ function Signup() {
     <article
       className={`${styles.logcard} ${showSign ? styles.showcard : styles.hiddencard}`}
     >
-      <h2 tabIndex="0" className={styles.cardtitle} onClick={toggleSign}>
-        <span>&#11206;</span> Sign Up
+      <h2 onClick={toggleSign}>
+        <button className={styles.cardtitle}>
+          <span>&#11206;</span> Sign Up
+        </button>
       </h2>
       <form
         className={`${styles.logform} ${showSign ? styles.showform : styles.hiddenform}`}
       >
-        <label htmlFor="pseudo">Pseudo</label>
-        <input id="pseudo" name="pseudo" type="text" />
+        <label tabIndex={showSign ? "0" : "-1"} htmlFor="pseudo">
+          Pseudo
+        </label>
+        <input
+          tabIndex={showSign ? "0" : "-1"}
+          id="pseudo"
+          name="pseudo"
+          type="text"
+        />
 
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" />
+        <label tabIndex={showSign ? "0" : "-1"} htmlFor="email">
+          Email
+        </label>
+        <input
+          tabIndex={showSign ? "0" : "-1"}
+          id="email"
+          name="email"
+          type="email"
+        />
 
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
+        <label tabIndex={showSign ? "0" : "-1"} htmlFor="password">
+          Password
+        </label>
+        <input
+          tabIndex={showSign ? "0" : "-1"}
+          id="password"
+          name="password"
+          type="password"
+        />
 
-        <label htmlFor="password">Confirm password</label>
-        <input id="confirm_password" name="confirm_password" type="password" />
+        <label tabIndex={showSign ? "0" : "-1"} htmlFor="password">
+          Confirm password
+        </label>
+        <input
+          tabIndex={showSign ? "0" : "-1"}
+          id="confirm_password"
+          name="confirm_password"
+          type="password"
+        />
 
-        <button type="button" className={styles.logbtn}>
+        <button
+          tabIndex={showSign ? "0" : "-1"}
+          type="button"
+          className={styles.logbtn}
+        >
           Sign Up
         </button>
       </form>
